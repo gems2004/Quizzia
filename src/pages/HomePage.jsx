888c33fa536950086ac326fb44ba5591c342adbd
Edmon <ed.moussa4@gmail.com>
2024-04-08 18:38:08 +0300

    simplified home page and fixed unwanted api call

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useGetUserByIdQuery } from "../features/users/usersApiSlice";
-import ErrorToken from "../components/ErrorToken";
-import { selectRole, selectTeacherId } from "../features/session/sessionSlice";
+import { selectRole } from "../features/session/sessionSlice";
 import { useSelector } from "react-redux";
-import QuizStartRequest from "../components/QuizStartRequest";
+import AdminUi from "./admin/AdminUi";
+import TeacherUi from "./teacher/TeacherUi";
+import StudentUi from "./student/StudentUi";
 
 function HomePage() {
-  const teacher_id = useSelector(selectTeacherId);
   const role = useSelector(selectRole);
-  const { data: info, isError, error } = useGetUserByIdQuery(teacher_id);
-
-  if (isError && error.status === 401) return <ErrorToken />;
 
   return (
     <div className="my-4 container d-flex flex-column align-items-center gap-4">
@@ -26,73 +21,12 @@ function HomePage() {
           </p>
         </div>
       </div>
-      {role === "Teacher" ? (
-        <div className="card" style={{ maxWidth: "28rem" }}>
-          <div className="card-body">
-            <h5 className="card-title">اختبار</h5>
-            <p className="card-text">
-              هنا يمكنك انشاء اختبار جديد او تصفح قائمة الاختبارات المنشأة
-              سابقاً.
-            </p>
-            <div className="d-flex gap-2 flex-wrap">
-              <Link to="/quiz/create" className="btn btn-primary">
-                انشاء اختبار
-              </Link>
-              <Link to="/quiz" className="btn btn-outline-primary">
-                تصفح الاختبارات
-              </Link>
-            </div>
-          </div>
-        </div>
-      ) : (
-        role === "Student" && (
-          <div className="card" style={{ maxWidth: "28rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">إجراء اختبار</h5>
-              <p className="card-text">
-                قم بإدخال الرمز الذي ارسله لك استاذك كي تقوم ببدء حل الاختبار
-                بعد قبول الطلب من قبل استاذك
-              </p>
-              <QuizStartRequest />
-            </div>
-          </div>
-        )
-      )}
       {role === "" ? (
-        <Link to="/admin" className="btn btn-outline-secondary">
-          Admin Dashboard
-        </Link>
+        <AdminUi />
+      ) : role === "Teacher" ? (
+        <TeacherUi />
       ) : (
-        <div className="card" style={{ maxWidth: "28rem" }}>
-          <div className="card-body">
-            <h5 className="card-title">معلومات</h5>
-            {role === "Student" ? (
-              <>
-                <p className="card-text">
-                  بعض المعلومات البسيطة عن الحساب الشخصي للطالب وانجازاته.
-                </p>
-                <div className="list-group list-group-flush">
-                  <div className="list-group-item">
-                    عدد الاختبارات المُنجزة:{" "}
-                    <span className="text-muted">{info?.no_of_quizzes}</span>
-                  </div>
-                </div>
-              </>
-            ) : (
-              <>
-                <p className="card-text">
-                  بعض المعلومات البسيطة عن الحساب الشخصي للاستاذ وطلابه.
-                </p>
-                <div className="list-group list-group-flush">
-                  <div className="list-group-item">
-                    عدد الاختبارات المنشأة:{" "}
-                    <span className="text-muted">{info?.no_of_quizzes}</span>
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
-        </div>
+        <StudentUi />
       )}
     </div>
   );
