f9a16fbad7b37f45df8790ece29e860d68f394fb
Edmon <ed.moussa4@gmail.com>
2024-04-11 17:37:43 +0300

    un rendered requests longer than 90 mins

--- src/pages/student/StudentRequestsPage.jsx
+++ src/pages/student/StudentRequestsPage.jsx
@@ -1,14 +1,30 @@
-import React from "react";
-import { useGetStudentRequestQuery } from "../../features/quizzes/quizApiSlice";
+import React, { useState } from "react";
+import {
+  useGetStudentRequestQuery,
+  useStartQuizMutation,
+} from "../../features/quizzes/quizApiSlice";
 import moment from "moment";
+import Spinner from "../../components/Spinner";
+import { useNavigate } from "react-router-dom";
 
 function StudentRequestsPage() {
-  const { data: requests } = useGetStudentRequestQuery();
-  console.log(requests);
+  const { data: requests, isLoading } = useGetStudentRequestQuery();
+  const [startQuiz] = useStartQuizMutation();
+
+  const navigate = useNavigate();
+
+  const [numOfUnRenderedElements, setNumOfUnRenderedElements] = useState(0);
+
+  async function startingQuiz(quiz_id) {
+    await startQuiz();
+    navigate(`/quiz/${quiz_id}/0`);
+  }
+
+  if (isLoading) return <Spinner />;
   return (
-    <div className="container my-4">
+    <div className="container my-4" id="requests-list">
       <h4 className="my-4">قائمة الطلبات:</h4>
-      {requests <= 0 ? (
+      {requests.length <= 0 ? (
         <p className="lead">لا يوجد طلبات</p>
       ) : (
         <div className="list-group">
@@ -16,6 +32,11 @@ function StudentRequestsPage() {
             const request_date = moment(request.created_at);
             const now = moment();
             const dif = now.diff(request_date, "minutes");
+
+            if (dif > 90) {
+              // setNumOfUnRenderedElements((prev) => prev + 1);
+              return;
+            }
             return (
               <div
                 className="list-group-item d-flex align-items-center justify-content-around "
@@ -61,6 +82,8 @@ function StudentRequestsPage() {
                       ? "disabled btn-outline-secondary"
                       : "btn-outline-success"
                   }`}
+                  onClick={() => startingQuiz(request.quiz_data.id)}
+                  // to={`/quiz/${request.quiz_data.id}/0`}
                 >
                   بدء الاختبار
                 </button>
